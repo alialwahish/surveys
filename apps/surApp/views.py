c32f247ecec6e67d557f49579a1c67747a0875a2
Ali Bayati <bayati.ali@icloud.com>
2018-05-13 17:18:00 -0500

    tried using session

--- apps/surApp/views.py
+++ apps/surApp/views.py
@@ -2,17 +2,18 @@ from django.shortcuts import render,redirect
 context={}
 i=0
 def index(request):
+    request.session['lastName']='albayati'
+
     return render(request,'index.html')
 
 def process(request):
-    
+        
     global i,context
     i+=1
     print("We're in process")
     if request.method=='POST':
         request.POST
         print(request.POST)
-        print(request.POST['name'])
         context={
             'name':request.POST['name'],
             'dojoName':request.POST['dojoName'],
